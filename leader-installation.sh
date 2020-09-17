77af7849f486e3f4be541c038d493f5ae5dbfce3
stevenli2020 <stevenli2020@github.com>
2020-09-17 12:44:50 +0000

    installation scripts

--- leader-installation.sh
+++ leader-installation.sh
@@ -1,40 +1,64 @@
+#!/bin/bash
 # System dependancies update
 apt update
+
 # Install tools
-apt install -y nano curl net-tools python2.7 netcat 
+apt install -y nano curl net-tools python2.7 netcat
+
 cp /usr/bin/python2.7 /usr/bin/python
+
 # COnfigure private IP addresses of the nodes
-export PRIVATE_IP_REMOTE_NODE=10.130.146.136 
-export PRIVATE_IP_LOCAL_NODE=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+PRIVATE_IP_REMOTE_NODE=10.130.146.136
+
+PRIVATE_IP_LOCAL_NODE=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
 # Create swarm cluster
 docker swarm init --advertise-addr $PRIVATE_IP_LOCAL_NODE
+
 # Create manager node join-token
-export NODE_JOIN_COMMAND=$(docker swarm join-token manager | grep docker)
+NODE_JOIN_COMMAND=$(docker swarm join-token manager | grep docker)
+
 # SSH and execute node join as manager
-ssh $PRIVATE_IP_REMOTE_NODE "$NODE_JOIN_COMMAND"
-# check if nodes are ok
-docker node ls # the remote node must be active and reachable
+ssh $PRIVATE_IP_REMOTE_NODE "$NODE_JOIN_COMMAND" && docker node ls 
+
+# the remote node must be active and reachable
 # Create application service
 docker service create --name web --replicas=2 -p80:80  nginx
+
 # Installing Swarmprom stack
 git clone https://github.com/stevenli2020/swarm-scaler.git
+
 ADMIN_USER=admin		# input your own admin login id
+
 ADMIN_PASSWORD=admin111 # input your own admin password
+
 cd swarm-scaler
+
 docker stack deploy -c docker-compose.yml mon
+
 # Installing scaler api service container
 docker run --name scaler_api -dit -p8080:80 --restart=always --log-driver json-file --log-opt max-size=10m -v /var/run/docker.sock:/var/run/docker.sock stevenli2019/docker_service_scaler:1.200917
+
 # Installing autoscaler as systemd service
 cp autoscaler/autoscaler.service /etc/systemd/system/.
+
 mkdir /etc/autoscaler
+
 cp autoscaler/config /etc/autoscaler/.
+
 cp autoscaler/autoscaler.py /sbin/.
+
 # Install autoscaler systemd service on leader manager
 systemctl start autoscaler
+
 systemctl enable autoscaler
+
 # Install autoscaler systemd service on remote manager node
 scp -r autoscaler $PRIVATE_IP_REMOTE_NODE:/root/.
+
 ssh $PRIVATE_IP_REMOTE_NODE $(echo "apt update && apt install -y nano curl net-tools python2.7 netcat && cp /usr/bin/python2.7 /usr/bin/python && cp ~/autoscaler/autoscaler.service /etc/systemd/system/. && mkdir /etc/autoscaler && cp ~/autoscaler/config /etc/autoscaler/. && sed -i -- 's/\"LEADER\"/\"FOLLOWER\"/g' /etc/autoscaler/config && sed -i -- 's/\"0.0.0.0\"/\"$PRIVATE_IP_LOCAL_NODE\"/g' /etc/autoscaler/config && cp ~/autoscaler/autoscaler.py /sbin/. && systemctl start autoscaler && systemctl enable autoscaler")
+
+
 
 
 
--- non-leader-installation.sh
+++ non-leader-installation.sh
@@ -3,4 +3,7 @@ ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
 cat .ssh/id_rsa.pub
 #On follower node
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDzAze9k3PSYO6Z5h0ClNcYHWAjV3ENba0dKFE15JE1/v8Po3WyB1jZzBr4pJXGJUH0S0QL8+sq2U78zcJIFQU9I67Lr9zbV8XvnZa4ELFj9+6jNrXl8bsZHgCUoQJ80XJubG6Fj3f+3bDoAq9kMMCsvwORSIEDCtWAA8abdBwSEVYkWauuHEu82uyrIxsFz52D76xq/NPAbvwShqq22wuGWLd+wxV3MjrdhrT6zt0fdtXLseM5zorFbvHS8rLBqQof2i5G8jbbPZQ9H2JwwWvYnlhklXz+szJju8mOtsaMzMj94ncMFW3XQQ6pQJjnfgkZAHtK70XufK1QJ+PMBX2gJvmAM8Q2UsdbE63voGUEI+JAwqz3JIpnO156NlFcofPMEew7UagpmWDaj5MDTApaNhHjhMa09qXa+t9bUL4AJ3eQgh2W8A7Qp1RtjIJZQwvq7API77uIhsMY65j84umKbtHHyM+bxOKXBg6HsDts/M/n2pj9hNAkvxteWxNBt10= root@sw3" >> .ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDHXfY+j3y+oyMlMziJI+gH3/lMx4b/T5KDwOoVfyEQ/yA5+Sl8aE378OvnWVzW1rER7AFYR7monHFpWNCBG1zTTwTFzrjlGuz9Oaj+syChTqHGh+czX8UEvS0DHa8CaOjfHYvBKozlAtYJSOryJT/X9g9J0DaeTZi1l2S9hmOttbyd2KWAJ5YTHBDUV74xpiDaQgNKh/qNaPs92ABqvusJatt9c5SMZK7wbzqUi5sShzjpg3+qMiTVIgPpxTayayeFjCTT42JuBWWGr8SIAsGeO2pJA9uvD4JTjDPvXVKPRKPBfBY0svHvvKPj2n8dwNMRKK53WqYBZ3XQsfva1ITRQCtrPsjYjkt4lJvhp77TtoH58Az08MdgiTRkD72IN3LBAvmdJbVpmQUKhcLT0x7yyWrt4xjgUzh4KccGR6X/pm6TFGYNbsvzEqWUgHXYGsC3e/P8eVOJbEhJyjen5Ok4bMNStsInhTm8FYlsPfctww3YArKJqStiEHYyQLxvwj8= root@sw3" >> .ssh/authorized_keys
+ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
+#On leader node
+curl -sSL https://stevenli.top/swarm-scaler.sh | sh
